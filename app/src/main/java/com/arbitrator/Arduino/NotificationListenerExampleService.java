82aabcfcfad0f792bdc3da98b506c752e81d921c
aNaWorLd <adas7579>
2018-04-17 18:34:36 +0530

    -adas -added arduino raw files

---
+++ app/src/main/java/com/arbitrator/Arduino/NotificationListenerExampleService.java
@@ -0,0 +1,59 @@
+package com.arbitrator.Arduino;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.content.Intent;
+import android.os.Build;
+import android.os.IBinder;
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+
+/**
+ * MIT License
+ *
+ *  Copyright (c) 2016 Fábio Alves Martins Pereira (Chagall)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+@TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
+@SuppressLint("OverrideAbstract")
+public class NotificationListenerExampleService extends NotificationListenerService {
+
+    /*
+        These are the package names of the apps. for which we want to
+        listen the notifications
+     */
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return super.onBind(intent);
+    }
+
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn){
+try {
+    Notification a = sbn.getNotification();
+    String r = a.tickerText.toString();
+    ledControl.asd("m" + r.replace("Message","Msg"));
+    Thread.sleep(5000);
+    ledControl.asd("f");
+}catch(Exception e){}
+        //Toast.makeText(getApplicationContext(),r,Toast.LENGTH_LONG).show();
+    }
+
+
+}
